d2a69bf6ec8d0ddb8b6e16f8a38d68fb14609e41
Dmitry <ermolaev@cvisionlab.com>
2017-03-20 14:29:36 +0300

    Add data reading

---
+++ ciraf.py
@@ -0,0 +1,38 @@
+"""
+    Module with functions for reading CIRAF data.
+"""
+
+def read_ciraf_file(file_name):
+    """
+        Read all data from CIRAF-file.
+    """
+    import cPickle
+    file_desc = open(file_name, 'rb')
+    result = cPickle.load(file_desc)
+    file_desc.close()
+    return result
+
+def merge_dicts(*dict_args):
+    """
+        Given any number of dicts, shallow copy and merge into a new dict,
+        precedence goes to key value pairs in latter dicts.
+    """
+    result = {}
+    for dictionary in dict_args:
+        result.update(dictionary)
+    return result
+
+def read_ciraf_10(files_path):
+    """
+        Read CIRAF-10 data in the path.
+        Returns list of training batches and test batch.
+        Labels in dict are: 'data', 'labels', 'batch_label', 'filenames'
+    """
+    data_batch_1 = read_ciraf_file(files_path + "/data_batch_1")
+    data_batch_2 = read_ciraf_file(files_path + "/data_batch_2")
+    data_batch_3 = read_ciraf_file(files_path + "/data_batch_3")
+    data_batch_4 = read_ciraf_file(files_path + "/data_batch_4")
+    data_batch_5 = read_ciraf_file(files_path + "/data_batch_5")
+    test_batch = read_ciraf_file(files_path + "/test_batch")
+
+    return [data_batch_1, data_batch_2, data_batch_3, data_batch_4, data_batch_5], test_batch
