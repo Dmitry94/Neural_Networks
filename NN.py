f0744114868e25b8cec764f9d259e0e67daf2f40
Dmitry <ermolaev@cvisionlab.com>
2017-03-20 15:20:10 +0300

    Finished NN-classifier

--- NN.py
+++ NN.py
@@ -2,6 +2,7 @@
     Nearest Neighbor Classifier demonstration.
 """
 
+import time
 import numpy as np
 import utils as ut
 import ciraf as cr
@@ -20,7 +21,12 @@ class NearestNeighborClassifier(object):
     def train(self, train_batches):
         """
             Train classifier on batches array.
-            Every batch should contains 'data' and 'label' fields
+
+            Parameters:
+            -------
+            train_batches : list[dictionary]
+                It's list of batches, each batch - dictionary.
+                Each dictionary must contains data and labels.
         """
         for batch in train_batches:
             batch_data = batch['data']
@@ -28,15 +34,46 @@ class NearestNeighborClassifier(object):
 
             if self.data.shape != batch_data.shape:
                 self.data = batch_data
-                self.labels = batch_labels
+                self.labels = np.array(batch_labels)
             else:
                 self.data = np.concatenate((self.data, batch_data), axis=0)
                 self.labels = np.concatenate((self.labels, batch_labels), axis=0)
 
+    def predict(self, test):
+        """
+            Assign labels for new data.
+
+            Parameters:
+            -------
+            test : np.array[uint8]
+                Array on DxN elements, where each row - sample,
+                for each we have to assign label.
+        """
+        samples_count = test.shape[0]
+        predictions = np.zeros(samples_count, dtype=self.labels.dtype)
+
+        for i in xrange(samples_count):
+            distances = ut.calc_l1(test[i, :], self.data)
+            min_index = np.argmin(distances)
+            predictions[i] = self.labels[min_index]
+
+        return predictions
 
 
 if __name__ == "__main__":
-    TRAIN_BATCHES, TEST_BATCH = cr.read_ciraf_10("content/ciraf/cifar-10-batches-py")
+    TRAIN_BATCHES, TEST_BATCH = cr.read_ciraf_10("content/ciraf/cifar-10-batches-py", 1)
 
     classifier = NearestNeighborClassifier()
+
+    start = time.clock()
     classifier.train(TRAIN_BATCHES)
+    end = time.clock()
+    print 'Training time = ', end - start
+
+    start = time.clock()
+    predictions = classifier.predict(TEST_BATCH['data'])
+    end = time.clock()
+    print 'Prediction time = ', end - start
+
+    accuracy = np.mean(predictions == TEST_BATCH['labels'])
+    print 'NN Accuracy = %f' % accuracy
