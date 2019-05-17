22fee489113eedc83f2dfe5e8f7a9768741e4494
Dmitry <DimonErmolaev1994@gmail.com>
2019-05-17 15:45:22 +0300

    Rename cifar reading utils file and fix it for python3.6

--- cifar_read_utils.py
+++ cifar_read_utils.py
@@ -1,17 +1,18 @@
 """
     Module with functions for reading CIRAF data.
 """
-import numpy as np
+
 
 def read_ciraf_file(file_name):
     """
         Read all data from CIRAF-file.
     """
-    import cPickle
+    import pickle
     file_desc = open(file_name, 'rb')
-    result = cPickle.load(file_desc)
+    result = pickle.load(file_desc, encoding='latin1')
     file_desc.close()
     return result
+
 
 def merge_dicts(*dict_args):
     """
@@ -23,22 +24,24 @@ def merge_dicts(*dict_args):
         result.update(dictionary)
     return result
 
+
 def read_ciraf_10(files_path, count=-1):
     """
         Read CIRAF-10 data in the path.
         Returns list of training batches and test batch.
         Labels in dict are: 'data', 'labels', 'batch_label', 'filenames'
     """
-    data_batch_1 = read_ciraf_file(files_path + "/data_batch_1")
-    data_batch_2 = read_ciraf_file(files_path + "/data_batch_2")
-    data_batch_3 = read_ciraf_file(files_path + "/data_batch_3")
-    data_batch_4 = read_ciraf_file(files_path + "/data_batch_4")
-    data_batch_5 = read_ciraf_file(files_path + "/data_batch_5")
+    data_batch_1 = read_ciraf_file(files_path + '/data_batch_1')
+    data_batch_2 = read_ciraf_file(files_path + '/data_batch_2')
+    data_batch_3 = read_ciraf_file(files_path + '/data_batch_3')
+    data_batch_4 = read_ciraf_file(files_path + '/data_batch_4')
+    data_batch_5 = read_ciraf_file(files_path + '/data_batch_5')
 
-    test_batch = read_ciraf_file(files_path + "/test_batch")
-    train_batches = [data_batch_1, data_batch_2, data_batch_3, data_batch_4, data_batch_5]
+    test_batch = read_ciraf_file(files_path + '/test_batch')
+    train_batches = [data_batch_1, data_batch_2, data_batch_3,
+                     data_batch_4, data_batch_5]
 
-    if count >= 0 and count < len(train_batches):
+    if len(train_batches) > count >= 0:
         train_batches = train_batches[0: count]
 
     return train_batches, test_batch
